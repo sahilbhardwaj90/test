44c5ba3fc607c111522bac68be6ea256e10be839
Sahil <sahibhardwaj0802@gmail.com>
2022-01-31 01:15:14 -0500

    add a python file

---
+++ classes.py
@@ -0,0 +1,11 @@
+# Program starts here...
+
+class Node():
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+if __name__ == "__main__":
+    Node(20,30)
+
+# Program ends here...
